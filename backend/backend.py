74b7a170f32dee6596594f50946380de48f8bb4b
Valentin Viennot <valentin.viennot@canonical.com>
2021-11-28 23:21:41 +0100

    take model as parameter

--- backend/backend.py
+++ backend/backend.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
-from flask import Flask, json, request, send_file, Response
+import os
+
+from flask import Flask, Response, abort, request
 from flask_cors import CORS
+
 from colorize import colorize_image
 
 api = Flask(__name__)
 CORS(api)
-    
-@api.route('/image', methods=['POST'])
-def post_image():
-  # check if the post request has the file part
-  if 'image' not in request.files:
-    return "Please send a file with key=image"
-  file = request.files['image']
-  version = request.args.get('version')
-  colorized_image=colorize_image(file)
-  return Response(colorized_image, mimetype="image/jpeg")
+
+
+@api.route('/colorize', methods=['POST'])
+def post_image(models={
+    'v1': (os.getenv("grpc_address_v1", '127.0.0.1'), os.getenv("grpc_port_v1", '9001')),
+    'v2': (os.getenv("grpc_address_v2", '127.0.0.1'), os.getenv("grpc_port_v2", '9002'))
+}):
+    version = request.args.get('version')
+    if version not in models:
+        abort(404, "This model doesn't exist")
+    if 'image' not in request.files:
+        abort(400, "Please send a file with key=image")
+    file = request.files['image']
+    return Response(colorize_image(file, models[version]), mimetype="image/jpeg")
+
 
 if __name__ == '__main__':
     api.run()
--- backend/colorize.py
+++ backend/colorize.py
@@ -1,78 +1,79 @@
-import os
 import cv2 as cv
 import numpy as np
 import grpc
-import datetime
 import tensorflow as tf
 from tensorflow import make_tensor_proto, make_ndarray
 from tensorflow_serving.apis import prediction_service_pb2_grpc, predict_pb2
-from helpers import model_metadata_response, get_model_metadata
+from helpers import get_model_metadata
+
 
 def convert_to_opencv(file, cv2_img_flag=cv.IMREAD_ANYCOLOR):
-  img_array = np.asarray(bytearray(file.read()), dtype=np.uint8)
-  return cv.imdecode(img_array, cv2_img_flag)
+    img_array = np.asarray(bytearray(file.read()), dtype=np.uint8)
+    return cv.imdecode(img_array, cv2_img_flag)
+
 
 def convert_to_bytes(open_cv_img):
-  _, buffer = cv.imencode('.jpg', open_cv_img)
-  return buffer.tobytes()
+    _, buffer = cv.imencode('.jpg', open_cv_img)
+    return buffer.tobytes()
 
-def colorize_image(file, SHAPE=256):
-  original_frame=convert_to_opencv(file)
 
-  (h_orig, w_orig) = original_frame.shape[:2]
-  
-  grpc_address= os.getenv("grpc_address", '127.0.0.1')
-  grpc_port=os.getenv("grpc_port", '9001')
-  address = "{}:{}".format(grpc_address,grpc_port)
-  
-  channel = grpc.insecure_channel(address)
-  stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
-  model_name='colorization-v2'
-  
-  input_metadata, output_metadata = get_model_metadata(stub, model_name)
-  input_shape=input_metadata['data_l']['shape']
-  
-  output_shape=output_metadata['color_ab']['shape']
-  _, _, h_in, w_in = input_shape
-  print('input shape', original_frame.shape)
+def colorize_image(file, model):
+    original_frame = convert_to_opencv(file)
 
-  if original_frame.shape[2] > 1:
-      frame = cv.cvtColor(cv.cvtColor(original_frame, cv.COLOR_BGR2GRAY), cv.COLOR_GRAY2RGB)
-  else:
-      frame = cv.cvtColor(original_frame, cv.COLOR_GRAY2RGB)
+    (h_orig, w_orig) = original_frame.shape[:2]
 
-  img_rgb = frame.astype(np.float32) / 255
-  img_lab = cv.cvtColor(img_rgb, cv.COLOR_RGB2Lab)
-  img_l_rs = cv.resize(img_lab.copy(), (w_in, h_in))[:, :, 0]
-  
-  batch_size=1
+    address = "{}:{}".format(model[0], model[1])
 
-  request = predict_pb2.PredictRequest()
-  request.model_spec.name = model_name
+    channel = grpc.insecure_channel(address)
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+    model_name = 'colorization-v2'
 
-  for input_name in input_metadata:
-    request.inputs[input_name].CopyFrom(make_tensor_proto(np.zeros(input_metadata[input_name]['shape']), dtype=tf.dtypes.float32))
+    input_metadata, output_metadata = get_model_metadata(stub, model_name)
+    input_shape = input_metadata['data_l']['shape']
 
-  request.inputs['data_l'].CopyFrom(make_tensor_proto(img_l_rs, shape=(1,1,256,256)))
-  start_time = datetime.datetime.now()
-  result = stub.Predict(request, 10.0) # result includes a dictionary with all model outputs
-  end_time = datetime.datetime.now()
+    output_shape = output_metadata['color_ab']['shape']
+    _, _, h_in, w_in = input_shape
+    print('input shape', original_frame.shape)
 
-  if 'color_ab' not in result.outputs:
-      print("Available outputs:")
-      for Y in result.outputs:
-          print(Y)
-      exit(1)
-      
-  duration = (end_time - start_time).total_seconds() * 1000
+    if original_frame.shape[2] > 1:
+        frame = cv.cvtColor(cv.cvtColor(
+            original_frame, cv.COLOR_BGR2GRAY), cv.COLOR_GRAY2RGB)
+    else:
+        frame = cv.cvtColor(original_frame, cv.COLOR_GRAY2RGB)
 
-  res = make_ndarray(result.outputs['color_ab'])
+    img_rgb = frame.astype(np.float32) / 255
+    img_lab = cv.cvtColor(img_rgb, cv.COLOR_RGB2Lab)
+    img_l_rs = cv.resize(img_lab.copy(), (w_in, h_in))[:, :, 0]
 
-  update_res = np.squeeze(res)
+    batch_size = 1
 
-  out = update_res.transpose((1, 2, 0))
-  out = cv.resize(out, (w_orig, h_orig))
-  img_lab_out = np.concatenate((img_lab[:, :, 0][:, :, np.newaxis], out), axis=2)
-  img_bgr_out = np.clip(cv.cvtColor(img_lab_out, cv.COLOR_Lab2BGR), 0, 1) * 255
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = model_name
 
-  return convert_to_bytes(img_bgr_out)
+    for input_name in input_metadata:
+        request.inputs[input_name].CopyFrom(make_tensor_proto(
+            np.zeros(input_metadata[input_name]['shape']), dtype=tf.dtypes.float32))
+
+    request.inputs['data_l'].CopyFrom(
+        make_tensor_proto(img_l_rs, shape=(1, 1, 256, 256)))
+    # result includes a dictionary with all model outputs
+    result = stub.Predict(request, 10.0)
+
+    if 'color_ab' not in result.outputs:
+        print("Available outputs:")
+        for Y in result.outputs:
+            print(Y)
+        exit(1)
+
+    res = make_ndarray(result.outputs['color_ab'])
+
+    update_res = np.squeeze(res)
+
+    out = update_res.transpose((1, 2, 0))
+    out = cv.resize(out, (w_orig, h_orig))
+    img_lab_out = np.concatenate(
+        (img_lab[:, :, 0][:, :, np.newaxis], out), axis=2)
+    img_bgr_out = np.clip(cv.cvtColor(
+        img_lab_out, cv.COLOR_Lab2BGR), 0, 1) * 255
+
+    return convert_to_bytes(img_bgr_out)
