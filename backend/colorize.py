6f334130488ed5ec2a660e92c5d55a166a1750aa
Valentin Viennot <valentin.viennot@canonical.com>
2021-11-28 23:54:32 +0100

    refactor

--- backend/colorize.py
+++ backend/colorize.py
@@ -1,9 +1,10 @@
 import cv2 as cv
-import numpy as np
 import grpc
+import numpy as np
 import tensorflow as tf
-from tensorflow import make_tensor_proto, make_ndarray
-from tensorflow_serving.apis import prediction_service_pb2_grpc, predict_pb2
+from tensorflow import make_ndarray, make_tensor_proto
+from tensorflow_serving.apis import predict_pb2, prediction_service_pb2_grpc
+
 from helpers import get_model_metadata
 
 
@@ -17,62 +18,72 @@ def convert_to_bytes(open_cv_img):
     return buffer.tobytes()
 
 
-def colorize_image(file, model):
-    original_frame = convert_to_opencv(file)
-
-    (h_orig, w_orig) = original_frame.shape[:2]
-
+def connect_grpc_model(model):
     address = "{}:{}".format(model[0], model[1])
-
     channel = grpc.insecure_channel(address)
     stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
-    model_name = 'colorization-v2'
+    return stub
 
-    input_metadata, output_metadata = get_model_metadata(stub, model_name)
-    input_shape = input_metadata['data_l']['shape']
 
-    output_shape = output_metadata['color_ab']['shape']
-    _, _, h_in, w_in = input_shape
-    print('input shape', original_frame.shape)
-
-    if original_frame.shape[2] > 1:
-        frame = cv.cvtColor(cv.cvtColor(
-            original_frame, cv.COLOR_BGR2GRAY), cv.COLOR_GRAY2RGB)
-    else:
-        frame = cv.cvtColor(original_frame, cv.COLOR_GRAY2RGB)
-
-    img_rgb = frame.astype(np.float32) / 255
-    img_lab = cv.cvtColor(img_rgb, cv.COLOR_RGB2Lab)
-    img_l_rs = cv.resize(img_lab.copy(), (w_in, h_in))[:, :, 0]
-
-    batch_size = 1
-
+def prep_model_input(remote_model, model_name='colorization'):
+    input_metadata, _ = get_model_metadata(remote_model, model_name)
     request = predict_pb2.PredictRequest()
     request.model_spec.name = model_name
-
     for input_name in input_metadata:
         request.inputs[input_name].CopyFrom(make_tensor_proto(
             np.zeros(input_metadata[input_name]['shape']), dtype=tf.dtypes.float32))
+    input_shape = input_metadata['data_l']['shape']
+    return (request, input_shape)
 
-    request.inputs['data_l'].CopyFrom(
-        make_tensor_proto(img_l_rs, shape=(1, 1, 256, 256)))
-    # result includes a dictionary with all model outputs
-    result = stub.Predict(request, 10.0)
 
+def transform_to_grayscale(original_frame):
+    if original_frame.shape[2] > 1:
+        return cv.cvtColor(cv.cvtColor(
+            original_frame, cv.COLOR_BGR2GRAY), cv.COLOR_GRAY2RGB)
+    return cv.cvtColor(original_frame, cv.COLOR_GRAY2RGB)
+
+
+def transform_to_normal_lab(img_rgb):
+    return cv.cvtColor(img_rgb.astype(np.float32) / 255, cv.COLOR_RGB2Lab)
+
+
+def predict_result(request, remote_model):
+    result = remote_model.Predict(request, 10.0)
     if 'color_ab' not in result.outputs:
         print("Available outputs:")
         for Y in result.outputs:
             print(Y)
         exit(1)
-
     res = make_ndarray(result.outputs['color_ab'])
-
     update_res = np.squeeze(res)
+    return update_res.transpose((1, 2, 0))
 
-    out = update_res.transpose((1, 2, 0))
-    out = cv.resize(out, (w_orig, h_orig))
+
+def colorize_image(file, model):
+    original_frame = convert_to_opencv(file)
+    (h_orig, w_orig) = original_frame.shape[:2]
+
+    # connect to the remote AI/ML model
+    remote_model = connect_grpc_model(model)
+    (request, input_shape) = prep_model_input(remote_model)
+
+    # transform image to grayscale LAB and input size
+    h_in, w_in = input_shape[2:4]
+    gray_frame = transform_to_grayscale(original_frame)
+    img_lab = transform_to_normal_lab(gray_frame)
+    img_l_rs = cv.resize(img_lab.copy(), (w_in, h_in))[:, :, 0]
+
+    # send input image to model
+    request.inputs['data_l'].CopyFrom(
+        make_tensor_proto(img_l_rs, shape=input_shape))
+    result = predict_result(request, remote_model)
+
+    # resize predicted color pixels to original image
+    out = cv.resize(result, (w_orig, h_orig))
+    # combine original Light info with predicted colors info
     img_lab_out = np.concatenate(
         (img_lab[:, :, 0][:, :, np.newaxis], out), axis=2)
+    # back to RGB and 255-scale
     img_bgr_out = np.clip(cv.cvtColor(
         img_lab_out, cv.COLOR_Lab2BGR), 0, 1) * 255
 
