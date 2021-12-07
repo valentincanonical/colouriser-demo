ecf0a1f37b8b5be4a07da473e9dc6c6644a75a41
mzeglars <milosz.zeglarski@intel.com>
2021-12-07 16:58:20 +0100

    switch to ovmsclient

--- backend/colorize.py
+++ backend/colorize.py
@@ -1,14 +1,9 @@
 import os 
 
 import cv2 as cv
-import grpc
 import numpy as np
-import tensorflow as tf
 
-from tensorflow import make_ndarray, make_tensor_proto
-from tensorflow_serving.apis import predict_pb2, prediction_service_pb2_grpc
-
-from helpers import get_model_metadata
+from ovmsclient import make_grpc_client, make_tensor_proto
 
 
 def convert_to_opencv(file, cv2_img_flag=cv.IMREAD_ANYCOLOR):
@@ -21,22 +16,13 @@ def convert_to_bytes(open_cv_img):
     return buffer.tobytes()
 
 
-def connect_grpc_model(address=os.getenv("GRPC_ADDRESS", '127.0.0.1'), port=os.getenv("GRPC_PORT", '9001')):
-    address = "{}:{}".format(address, port)
-    channel = grpc.insecure_channel(address)
-    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
-    return stub
-
-
-def prep_model_input(remote_model, model_name):
-    input_metadata, _ = get_model_metadata(remote_model, model_name)
-    request = predict_pb2.PredictRequest()
-    request.model_spec.name = model_name
+def prep_model_input(model_metadata):
+    inputs = {}
+    input_metadata = model_metadata["inputs"]
     for input_name in input_metadata:
-        request.inputs[input_name].CopyFrom(make_tensor_proto(
-            np.zeros(input_metadata[input_name]['shape']), dtype=tf.dtypes.float32))
+        inputs[input_name] = np.zeros(input_metadata[input_name]['shape'], dtype=np.float32)
     input_shape = input_metadata['data_l']['shape']
-    return (request, input_shape)
+    return (inputs, input_shape)
 
 
 def transform_to_grayscale(original_frame):
@@ -50,25 +36,16 @@ def transform_to_normal_lab(img_rgb):
     return cv.cvtColor(img_rgb.astype(np.float32) / 255, cv.COLOR_RGB2Lab)
 
 
-def predict_result(request, remote_model):
-    result = remote_model.Predict(request, 10.0)
-    if 'color_ab' not in result.outputs:
-        print("Available outputs:")
-        for Y in result.outputs:
-            print(Y)
-        exit(1)
-    res = make_ndarray(result.outputs['color_ab'])
-    update_res = np.squeeze(res)
-    return update_res.transpose((1, 2, 0))
-
-
 def colorize_image(file, model_name):
     original_frame = convert_to_opencv(file)
     (h_orig, w_orig) = original_frame.shape[:2]
 
     # connect to the remote AI/ML model
-    remote_model = connect_grpc_model()
-    (request, input_shape) = prep_model_input(remote_model, model_name)
+    address=os.getenv("GRPC_ADDRESS", '127.0.0.1')
+    port=os.getenv("GRPC_PORT", '9001')
+    ovms_client = make_grpc_client("{}:{}".format(address, port))
+    model_metadata = ovms_client.get_model_metadata(model_name)
+    (inputs, input_shape) = prep_model_input(model_metadata)
 
     # transform image to grayscale LAB and input size
     h_in, w_in = input_shape[2:4]
@@ -77,9 +54,10 @@ def colorize_image(file, model_name):
     img_l_rs = cv.resize(img_lab.copy(), (w_in, h_in))[:, :, 0]
 
     # send input image to model
-    request.inputs['data_l'].CopyFrom(
-        make_tensor_proto(img_l_rs, shape=input_shape))
-    result = predict_result(request, remote_model)
+    inputs['data_l'] = make_tensor_proto(img_l_rs, shape=input_shape)
+    result = ovms_client.predict(inputs, model_name)
+    update_res = np.squeeze(result)
+    result = update_res.transpose((1, 2, 0))
 
     # resize predicted color pixels to original image
     out = cv.resize(result, (w_orig, h_orig))
